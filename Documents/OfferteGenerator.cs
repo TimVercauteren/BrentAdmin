13512ebc8427180ca5ae458fc606ce86824180b0
Tim Vercauteren <tvercauteren@barclab.com>
2019-11-20 15:16:07 +0100

    add some document shit

---
+++ Documents/OfferteGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Documents.DTO;
+using Documents.Interfaces;
+
+namespace Documents
+{
+    public class OfferteGenerator : IOfferteGenerator
+    {
+        public OfferteGenerator()
+        {
+
+        }
+
+        public static Workbook GetOfferteTemplate()
+        {
+            const string fullpath = "Templates\\BrentOfferteTemplate.xlsx";
+            var offerte = new Workbook(fullpath);
+            return offerte;
+        }
+
+        public void FillTemplateWithOfferteData(OfferteDTO dataObject)
+        {
+            var template = GetOfferteTemplate();
+
+
+
+        }
+
+    }
+}
